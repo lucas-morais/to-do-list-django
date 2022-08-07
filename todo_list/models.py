54ba02035752d760beb9230608f0c8d696dc6f0a
Lucas Toledo <lucasmoraisss@gmail.com>
2022-08-07 08:13:04 -0300

    feat: todo model

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    CATEGORY = (("L", "Leisure"), ("W", "Work"))
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    description = models.CharField(max_length=255, null=False, blank=False)
+    finished = models.BooleanField(default=False, null=False)
+    category = models.CharField(
+        max_length=1, choices=CATEGORY, null=False, blank=False
+    )
+    created_at = models.DateField(auto_now_add=True)
