1866d1f77f3562bd2142ef65c45179a9633d87d6
Lucas Toledo <lucasmoraisss@gmail.com>
2022-08-09 08:44:46 -0300

    feat: creat and get todo endpoints

--- todo_list/apis/todo_api.py
+++ todo_list/apis/todo_api.py
@@ -1,8 +1,19 @@
 from ninja import Router
+from ..schemas.todo_schema import TodoDTO, TodoForm
+from ..models.todo_models import Todo
+from typing import List
 
 router = Router()
 
 
-@router.get("/")
+@router.get("", response=List[TodoDTO])
 def hello(_request):
-    return {"message": "Hello, World!"}
+    todos = Todo.objects.all()
+    return todos
+
+
+@router.post("", response=TodoDTO)
+def create_todo(_request, todo_form: TodoForm):
+    todo = Todo(**todo_form.dict())
+    todo.save()
+    return todo
