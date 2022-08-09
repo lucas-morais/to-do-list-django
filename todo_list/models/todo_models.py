ed006a7df4c7f1944d1407a7e7b223fe94616d8f
Lucas Toledo <lucasmoraisss@gmail.com>
2022-08-09 08:45:15 -0300

    refactor: defining default category

--- todo_list/models/todo_models.py
+++ todo_list/models/todo_models.py
@@ -9,6 +9,6 @@ class Todo(models.Model):
     description = models.CharField(max_length=255, null=False, blank=False)
     finished = models.BooleanField(default=False, null=False)
     category = models.CharField(
-        max_length=1, choices=CATEGORY, null=False, blank=False
+        max_length=1, choices=CATEGORY, null=False, blank=False, default="L"
     )
     created_at = models.DateField(auto_now_add=True)
