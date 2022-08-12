2daac8b8796c64d293c4504c56c0a66d49bc2ed7
Lucas Toledo <lucasmoraisss@gmail.com>
2022-08-12 17:47:06 -0300

    reafcator: todo model using django enums

--- todo_list/models/todo_models.py
+++ todo_list/models/todo_models.py
@@ -1,15 +1,22 @@
 import uuid
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class Todo(models.Model):
-    CATEGORY = (("L", "Leisure"), ("W", "Work"))
+    class Category(models.TextChoices):
+        LEISURE = "L", _("Leisure")
+        WORK = "W", _("Work")
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     description = models.CharField(max_length=255, null=False, blank=False)
     finished = models.BooleanField(default=False, null=False)
     category = models.CharField(
-        max_length=1, choices=CATEGORY, null=False, blank=False, default="L"
+        max_length=1,
+        choices=Category.choices,
+        null=False,
+        blank=False,
+        default=Category.LEISURE,
     )
     created_at = models.DateField(auto_now_add=True)
 
