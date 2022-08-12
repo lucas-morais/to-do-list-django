465a4c36dee8a885e4de78d79f12a0fd4c1259b1
Lucas Toledo <lucasmoraisss@gmail.com>
2022-08-12 18:06:29 -0300

    feat: update todo endpoint

--- todo_list/apis/todo_api.py
+++ todo_list/apis/todo_api.py
@@ -1,6 +1,6 @@
 from ninja import Router
 from django.shortcuts import get_object_or_404
-from ..schemas.todo_schema import TodoDTO, TodoForm
+from ..schemas.todo_schema import TodoDTO, TodoForm, TodoUpdateForm
 from ..models.todo_models import Todo
 from typing import List
 
@@ -27,5 +27,10 @@ def list_by_id(_request, id: str):
 
 
 @router.put("/{str:id}", response=TodoDTO)
-def update(_request, id: str):
-    pass
+def update(_request, id: str, updateForm: TodoUpdateForm):
+    todo = get_object_or_404(Todo, id=id)
+    todo.category = updateForm.category
+    todo.description = updateForm.description
+    todo.finished = updateForm.finished
+    todo.save()
+    return todo
